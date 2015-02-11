cca58a09dd2ec71f2c8aaf35ee58aa8e23ed9cf0
SeongJae Park <sj38.park@gmail.com>
2015-02-12 03:08:31 +0900

    fix trivial typos
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 07_Scan/app/src/main/java/rx/android/gdg/kr/scan/MainActivity.java
+++ 07_Scan/app/src/main/java/rx/android/gdg/kr/scan/MainActivity.java
@@ -15,18 +15,18 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 플러스, 마이너스 버튼에 대해 각기 -1 값과 1 값을 스트림에 보냄.
+        // 플러스, 마이너스 버튼에 대해 각기 -1 값과 +1 값을 스트림에 보냄.
         Observable<Integer> minuses = ViewObservable.clicks(findViewById(R.id.minusButton))
                 .map(event -> -1);
         Observable<Integer> pluses = ViewObservable.clicks(findViewById(R.id.plusButton))
                 .map(event -> 1);
 
 
-        // -1과 1값을 한번에 하나의 스트림으로
+        // -1과 +1값을 한번에 하나의 스트림으로
         Observable<Integer> together = Observable.merge(minuses, pluses);
 
 
-        // scan의 첫번째 인자는 초기 값이고 두번째 인자에는 람다가 들어간다. 첫째 값은 누적된 값이며 두번째 값은
+        // scan의 첫번째 인자는 초기 값이고 두번째 인자에는 람다가 들어갑니다. 첫번째 값은 누적된 값이며 두번째 값은
         // 이번에 받은 값입니다. 리턴한 sum + 1의 값이 다음 번에 sum 인자로 넘어오게 됩니다.
         // 이 scan은 들어오는 이벤트의 수를 집계하기 위한 것입니다.
 
